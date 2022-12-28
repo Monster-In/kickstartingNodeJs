4c6a01bbc1cae711d7ad6d1204c8755c2e35d210
K <onemod@runorbit.com>
2022-12-28 12:22:56 +0530

    Add existing project files to Git

--- arrow-func.js
+++ arrow-func.js
@@ -38,12 +38,33 @@
 // const transformedArray = array.map(x => x === ' ' ? 'empty string' : x);
 // console.log(transformedArray);
 
+
+
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//  res.writeHead(200, { 'Content-Type': 'text/plain' });
+//  res.end('Hello World\n');
+// });
+
+// server.listen(4000, () => {
+//  console.log('Hi, I am Tej');
+// });
+
 const http = require('http');
+
 const server = http.createServer((req, res) => {
- res.writeHead(200, { 'Content-Type': 'text/plain' });
- res.end('Hello World\n');
+  if (req.url === '/home') {
+    res.end('Welcome home');
+  } else if (req.url === '/about') {
+    res.end('Welcome to About Us page');
+  } else if (req.url === '/node') {
+    res.end('Welcome to my Node Js project');
+  } else {
+    res.end('404 Not Found');
+  }
 });
 
-server.listen(4000, () => {
- console.log('Hi, I am Tej');
+server.listen(3000, () => {
+  console.log('Namasthe');
 });
+
