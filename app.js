e6b67eadbcebf077769eac4e3ef500a0cbe59781
K <onemod@runorbit.com>
2023-02-02 20:27:53 +0530

    Simple Group Chat Application

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const bodyParser=require('body-parser');
+const loginRoutes= require('./routes/admin');
+const messageRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({excluded:false}));
-app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+const bodyParser = require('body-parser');
 
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get("/", (req,res) => {
+    fs.readFile("username.txt", (err,data)=>{
+        if(err){
+            console.log(err);
+            data='No chat exists'
+        }
+        res.send(
+            `${data} <form action="/"
+            onsubmit="document.getElementById('username').value=localStorage.getItem('username')" method="POST">
+                    <input id="message" name="message" type="text">
+                    <input type="hidden" name="username" id="username">
+                    <br/>
+                    <button type="submit">send</button>
+                </form>`
+        );
+    })
 })
+
+app.post("/", (req,res) => {
+    console.log(req.body.username);
+    console.log(req.body.message);
+
+    fs.writeFile("username.txt",`${req.body.username} : ${req.body.message}`, {flag:'a'}, (err) => err ? console.log(err):res.redirect("/"));
+});
+
+app.get("/login", (req,res) => {
+    res.send(`<form action="/" method="POST" onsubmit="localStorage.setItem('username', document.getElementById('username').value)">
+            <input id="username" placeHolder="username" type="text" name="username">
+            <br/>
+            <button type="submit">login</button></form>`);
+});
 
 app.listen(4000);
