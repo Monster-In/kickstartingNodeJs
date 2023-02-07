1d62efaf1f5402490db6b0bcb83ca92cb5aeecfe
K <onemod@runorbit.com>
2023-02-07 11:31:07 +0530

    added models

--- controllers/product.js
+++ controllers/product.js
@@ -1,25 +1,20 @@
+const path = require("path");
+const rootDir = require("../util/path");
+
+const Product = require("../models/product.js");
+
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true
-    });
-  }
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+};
 
-  exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
-    res.redirect('/');
-  }
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
 
-  exports.getProducts = (req, res, next) => {
-    res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
-  }
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.sendFile(path.join(rootDir, "views", "shop.html"));
+  });
+};
